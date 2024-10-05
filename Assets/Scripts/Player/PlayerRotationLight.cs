e158ef4c2476f24603ba8d43818e2652783a1b2f
qwezert <qwezertino@gmail.com>
2024-10-05 23:03:13 +0300

    update navmesh + script to check enemy in range

--- Assets/Scripts/Player/PlayerRotationLight.cs
+++ Assets/Scripts/Player/PlayerRotationLight.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class PlayerRotationLight : MonoBehaviour
 {
+    [SerializeField] private float viewAngle =  75.0f;
+    [SerializeField] private GameObject enemiesContainer;
+    List<Transform> enemies = new List<Transform>();
+    private void Start()
+    {
+        // Get all child Transforms of the enemiesContainer
+        Transform[] enemyTransforms = enemiesContainer.GetComponentsInChildren<Transform>();
+
+        // Loop through the transforms and print their positions
+        foreach (Transform enemyTransform in enemyTransforms)
+        {
+            // Exclude the parent container itself
+            if (enemyTransform != enemiesContainer.transform)
+            {
+                enemies.Add(enemyTransform);
+                // Debug.Log("Enemy Position: " + enemyTransform.position);
+            }
+        }
+    }
     private void Update()
     {
+        foreach (Transform enemy in enemies)
+        {
+            if (!CheckInRange(new Vector2(enemy.position.x, enemy.position.y)))
+            {
+                return;
+            }
+        }
         HandleRotation();
     }
     private void HandleRotation()
@@ -14,15 +40,16 @@ public class PlayerRotationLight : MonoBehaviour
         Vector3 rotation = mousePosition - transform.position;
         float angle = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle);
-        // Vector3 localScale = Vector3.one;
-        // if (angle > 90 || angle < -90)
-        // {
-        //     localScale.y = -1f;
-        // }
-        // else
-        // {
-        //     localScale.y = 1f;
-        // }
-        // transform.localScale = localScale;
+
+    }
+    public bool CheckInRange(in Vector2 position)
+    {
+        Vector2 dir = position - new Vector2(transform.position.x, transform.position.y);
+        float dot = Vector2.Dot(new Vector2(transform.forward.x, transform.forward.y), dir.normalized);
+        if (dot < Mathf.Cos(viewAngle * 0.5f * Mathf.Deg2Rad))
+        {
+            return false;
+        }
+        return true;
     }
 }
