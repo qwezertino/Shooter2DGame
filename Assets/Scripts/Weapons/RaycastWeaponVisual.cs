0f1c74a2ca663746ea651684cf208b3d9a189b99
qwezert <qwezertino@gmail.com>
2025-12-14 16:04:36 +0200

    feat: Refactor weapon references to a unified naming convention across player and weapon prefabs

--- Assets/Scripts/Weapons/RaycastWeaponVisual.cs
+++ Assets/Scripts/Weapons/RaycastWeaponVisual.cs
@@ -5,20 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class RaycastWeaponVisual : MonoBehaviour
 {
-    [SerializeField] private RaycastWeapon _pistol;
+    [SerializeField] private RaycastWeapon _weapon;
     private const string ATTACK = "Attack";
     private Animator _animator;
     private void Awake()
     {
         _animator = GetComponent<Animator>();
     }
-
     private void Start()
     {
-        _pistol.OnAttack += OnPistolShot;
+        _weapon.OnAttack += OnWeaponShot;
     }
-
-    private void OnPistolShot(object sender, System.EventArgs e)
+    private void OnWeaponShot(object sender, System.EventArgs e)
     {
         _animator.SetTrigger(ATTACK);
     }
